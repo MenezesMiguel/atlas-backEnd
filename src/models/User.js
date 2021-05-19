310fbc84d5e217c8c29ba8904322e94382588315
Miguel Menezes <miguelmenezes99@hotmail.com>
2021-05-19 13:23:38 -0300

    Model User

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const connection = require("../database/connection");
+module.exports = {
+  async create() {
+    const result = await connection("user").insert(user);
+    return result;
+  },
+
+  async getById({ user_id }) {
+    const result = await connection("user").select("*");
+    return result;
+  },
+
+  async updateById(user_id, user) {
+    const result = await connection("user").where(user_id).update(user);
+    return result;
+  },
+  async deleteById(user_id) {
+    const result = await connection("user").where({ user_id }).delete();
+    return result;
+  },
+};

b170e25a20d350d1422beac95e225dd68b0169be
Miguel Menezes <miguelmenezes99@hotmail.com>
2021-05-19 00:17:41 -0300

    Config Firebase

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,2 +1,11 @@
 const firebase = require('firebase/app');
 require('firebase/auth');
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.STORAGE_BUCKET,
+    databaseURL: process.env.PROJECT_ID,
+    storageBucket: process.env.MESSAGING_SENDER_ID,
+    messagingSenderId: process.env.APP_ID
+}

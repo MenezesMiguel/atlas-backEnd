ad3037328b430a8a30a15707436ee9b1242e7b33
Miguel Menezes <miguelmenezes99@hotmail.com>
2021-05-19 16:49:17 -0300

    Get/Post/Put (Demonstracao)

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,71 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
 
-routes.get('/teste', (req, res) => {
-    console.log(req);
-    res.send("oi");
+const familiaPera = [
+  {
+    id: 0,
+    name: "Arthur Braga",
+  },
+  {
+    id: 1,
+    name: "Joaquim Jose",
+  },
+  {
+    id: 2,
+    name: "Miguel Menezes",
+  },
+  {
+    id: 3,
+    name: "Leticia Lopes",
+  },
+  {
+    id: 4,
+    name: "Maria Joaquina",
+  },
+  {
+    id: 5,
+    name: "Joao Paulo",
+  },
+  {
+    id: 6,
+    name: "Gilberto Assis",
+  },
+];
+
+routes.get("/home", (req, res) => {
+  res.json(familiaPera);
+});
+
+routes.post("/home", (req, res) => {
+  const newUser = req.body;
+
+  familiaPera.push(newUser);
+
+  res.json({ message: "Sucess" });
+});
+
+routes.put("/home/:user_Id", (req, res) => {
+  const { userId } = req.params;
+  const newFields = req.body;
+
+  let selectedIndex;
+  let selected = familiaPera.find((user, index) => {
+    selectedIndex = index;
+    return user.id === userId;
+  });
+  selected = { ...selected, ...newFields };
+
+  familiaPera[selectedIndex] = selected;
+  res.json({ message: "Sucess" });
+});
+
+routes.post("/home", (req, res) => {
+  res.send(familiaPera);
+});
+
+routes.post("/home", (req, res) => {
+  console.log(req);
+  res.send("oi");
 });
 
 module.exports = routes;

adc11e4c4a35d448f4f9d8dab4ed176b82b7a4d7
Miguel Menezes <miguelmenezes99@hotmail.com>
2021-05-24 09:41:30 -0300

    Autenticacao Login/ Token JWT

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -11,14 +11,12 @@ module.exports = {
   },
 
   async getById({ user_id }) {
-    const result = await connection("user").select("*");
+    const result = await connection("user").select("*").first();
     return result;
   },
 
   async updateById(user_id, user) {
-    const result = await connection("user")
-      .where({ user_id })
-      .update(user);
+    const result = await connection("user").where({ user_id }).update(user);
     return result;
   },
   async deleteById(user_id) {
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const routes = express.Router();
 
+const auth = require("./middlewares/authentication");
+
 const AddressController = require("./controllers/AddressController");
 const AddressValidator = require("./validators/AddressValidator");
 
 const UserController = require("./controllers/UserController");
 const UserValidator = require("./validators/UserValidator");
 
-const SessionController= require("./controllers/SessionController");
+const SessionController = require("./controllers/SessionController");
 
 //Sessao
-routes.post("/login", SessionController.login);
-
+routes.post("/login", SessionController.signIn);
 
 //users
 routes.get("/users/:user_id", UserValidator.getById, UserController.getById);
@@ -23,17 +24,20 @@ routes.delete("/users/:user_id", UserValidator.delete, UserController.delete);
 routes.get(
   "/users/:address_id",
   AddressValidator.getById,
+  auth.authenticateToken,
   AddressController.getById
 );
 routes.post("/address", AddressValidator.create, AddressController.create);
 routes.put(
   "/address/:address_id",
   AddressValidator.update,
+  auth.authenticateToken,
   AddressController.update
 );
 routes.delete(
   "/address/:address_id",
   AddressValidator.delete,
+  auth.authenticateToken,
   AddressController.delete
 );
 
